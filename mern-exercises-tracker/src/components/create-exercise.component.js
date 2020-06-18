deb641e12029cc0ce5aefd1a74df21eb6f49a4a0
sidsheng <sidsheng@gmail.com>
2020-06-18 13:39:13 +1000

    create user, edit/delete/list exercises

--- mern-exercises-tracker/src/components/create-exercise.component.js
+++ mern-exercises-tracker/src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class CreateExercises extends Component {
     constructor(props) {
@@ -22,10 +23,15 @@ export default class CreateExercises extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users')
+            .then(res => {
+                if (res.data.length > 0) {
+                    this.setState({
+                        users: res.data.map(user => user.username),
+                        username: res.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -62,6 +68,9 @@ export default class CreateExercises extends Component {
         }
 
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
 
         window.location = '/';
     }
